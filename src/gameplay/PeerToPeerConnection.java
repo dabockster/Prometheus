c7354c60d2d00c04d73deb0871533cbbc2cbc36f
Timothy Ernst <ernstta@plu.edu>
2015-04-28 08:29:50 -0700

    removed letter to Sean before he read it. fuck you Sean.

--- src/gameplay/PeerToPeerConnection.java
+++ src/gameplay/PeerToPeerConnection.java
@@ -8,23 +8,7 @@ import client.manager.ClientController;
  */
 public class PeerToPeerConnection {
     
-    //Dear Sean,
-    //
-    //     I have stubbed out some of the methods I know you will need
-    //because I am really on a role with stubbing. You might think its annoying
-    //or you might find that they are very helpful. If the prior is true
-    //feel free to delete anything except the ones I wrote for myself:)
-    //On a seperate note, I do not have my phone today (Jessicah does because
-    //she wants to play Hearthstone). I cannot blame her. It is a fun game
-    //afterall! Anyways I'm just sitting in the compuer lab with Steven. Just 
-    //me and him having the time of our lives! Wish you were here buddy,
-    //
-    //Good luck writing the rest of this thread! Let me know if you need any
-    //help or want to grab a beer.
-    //
-    //      Sincerely,
-    //         Mr. Timothy Ernst
-    
+
     
     /**
      * HOST CLIENT
